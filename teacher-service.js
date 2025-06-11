b520b1c3e050d4d0c23dd8171eba3767a50e1bb3
Sreytoch-Brak <137994391+sreytochbrak@users.noreply.github.com>
2025-06-11 16:32:42 +0700

    Update teacher-service.js

--- teacher-service.js
+++ teacher-service.js
@@ -8,4 +8,4 @@ app.post('/addassignment', (req, res) => res.send('Assignment added!'));
 app.get('/searchstudent', (req, res) => res.send('Student found!'));
 app.delete('/removestudent', (req, res) => res.send('Student removed!'));
 
-app.listen(3002, () => console.log('Teacher service running on 3002'));
+app.listen(5001, () => console.log('Teacher service running on 5001'));
